04f33b8778880e51c19f89af723a28d7ffc25391
Ilpo Ruotsalainen <lonewolf@iki.fi>
2024-02-06 13:07:36 -0800

    Improve redirect link syntax.

---
+++ pages/linkmaker.js
@@ -0,0 +1,36 @@
+function abort(reason) {
+    alert(reason);
+    throw 1;
+}
+
+function redirect() {
+    const link = document.querySelector('a');
+    const urlParams = new URLSearchParams(window.location.search);
+    const target = urlParams.get('target');
+    if (target) {
+        link.textContent = `Add ${target}`;
+        link.href = target;
+    } else {
+        const hostname = urlParams.get('hostname') ?? abort('Hostname must be set');
+        const port = urlParams.get('port') ?? abort('Port must be set');
+        const url = new URL(`acc-connect://${hostname}:${port}/`);
+
+        const name = urlParams.get('name');
+        const persistent = urlParams.get('persistent') == 'true';
+
+        var linkText;
+        if (name) {
+            url.searchParams.set('name', name);
+            linkText = `${name} (${hostname}:${port})`;
+        }
+        if (persistent) {
+            url.searchParams.set('persistent', persistent);
+            linkText = '\u2605' + linkText;
+        }
+        link.textContent = `Add ${linkText}`;
+        link.href = url.href;
+    }
+    link.click();
+}
+
+window.onload = redirect;
